a733d0adaad1f075f59df38c5ea94a7e859b860d
d-theus <slma0x02@gmail.com>
2016-05-28 20:55:05 +0300

    fixed postfix alias.db complaining

--- setup-postfix.sh
+++ setup-postfix.sh
@@ -3,4 +3,7 @@
 postconf -e myhostname=$DOMAIN
 postconf -e mydestination=smtp.$DOMAIN,mail.$DOMAIN,$DOMAIN
 postconf -e virtual_alias_domains=$DOMAIN
+
+touch /etc/postfix/aliases
+postalias hash:/etc/postfix/aliases
  
